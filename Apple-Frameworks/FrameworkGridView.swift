3ff52477cff7881b6c3edc0a08134b89ef82d786
shahviraj222 <viraj22feb@gmail.com>
2025-05-07 10:25:34 +0530

    a view framework title is created and added in main view

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,41 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by viraj shah on 07/05/25.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        FrameworkTitleView(name: "Car Play", imageName: "carplay")
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
+
+struct FrameworkTitleView:View{
+    
+    let name:String
+    let imageName:String
+    
+    var body: some View {
+        VStack{
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 90.0, height: 90.0)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.5)
+            
+        }
+    }
+
+    
+}
